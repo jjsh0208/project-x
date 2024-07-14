7f0bd4dede4c1e68f4a9fbb52e811b9e24610787
DDong_kka <jjsh0208@gmail.com>
2024-07-14 23:19:21 +0900

    VitaBiodome06.java 업데이트

--- src/step4_1/VitaBiodome06/VitaBiodome06.java
+++ src/step4_1/VitaBiodome06/VitaBiodome06.java
@@ -30,7 +30,6 @@ public class VitaBiodome06 extends HttpServlet {
         PrintWriter out = resp.getWriter();
 
         resp.setStatus(HttpServletResponse.SC_OK);
-//        out.println("{\"message\" : \"test\" "); 
 
     }
 
